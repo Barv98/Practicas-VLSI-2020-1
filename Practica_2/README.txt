06f2be1112e5c0759a34fd6075b18268318dbe8e
Barv98 <54648199+Barv98@users.noreply.github.com>
2019-09-12 20:40:12 -0500

    Add files via upload

---
+++ Practica_2/README.txt
@@ -0,0 +1,21 @@
+
+Integrantes:
+-Mendoza Guillén Sergio Adrián
+-Navarro Velázquez Jorge
+-Ramirez Villa Brandon Alberto
+-Vargas Badillo Ricardo Antonio
+
+Descripción:
+Un sistema de monitoreo de variables físicas está constituido por 8 sensores. Se desea implementar un sistema automático que permita 
+realizar la selección de 4 de los 8 sensores simult´aneamente en cada instante de tiempo, de tal forma que, en el primer instante, 
+se deberán activar en nivel alto los canales asociados a los sensores S0, S3, S4 y S7, donde S0 es el sensor asociado al LSB y S7 al MSB. 
+En el segundo instante deberán estar activos S2, S3, S6 y S7, y as´i sucesivamente hasta regresar al estado inicial, para repetir la 
+secuencia nuevamente. En la ?gura siguiente se muestran los estados de los sensores activos para el primer, segundo y tercer instante de 
+tiempo.
+
+Por otra parte, el diseño debe considerar que mediante un selector (switch) la dirección en que se corren los bits puede ser invertida 
+respecto al caso anterior. Finalmente, debe existir la posibilidad de regresar al sistema a su estado inicial mediante un botón de RESET 
+(señal asíncrona).
+
+Link:
+https://www.youtube.com/watch?v=NJGjKcnP2Xg&t=22s
---
+++ Practica_2/Registro_Corrimiento.vhd
@@ -0,0 +1,51 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY Registro_Corrimiento IS
+	PORT (mclk: IN STD_LOGIC;
+			DIR: IN STD_LOGIC;
+			RST: IN STD_LOGIC;
+			LEDS: OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+END Registro_Corrimiento;
+
+ARCHITECTURE Behavioral OF Registro_Corrimiento IS
+	
+		SIGNAL delay: INTEGER RANGE 0 TO 64000000:=0;                     --Esto debido al reloj base de 64 MHz
+		SIGNAL div: STD_LOGIC:='0';
+		SIGNAL corrimiento: STD_LOGIC_VECTOR (7 DOWNTO 0):= "10011000";
+	
+	BEGIN
+	--divisor @1 HZ
+	divisor: PROCESS(mclk)                                                --frec divisor = Reloj Base (mclk) / N => N =(64MHz / 1 Hz) / 2 -1 = 3199999
+	BEGIN
+		IF RISING_EDGE(mclk) THEN
+			IF (delay = 31999999) THEN                                      --el limite de cuenta para el Divisor 
+				delay<=0;
+				div<=not div;
+			ELSE
+				delay<=delay+1;
+			END IF;
+		END IF;
+	END PROCESS;
+	
+	registro: PROCESS (div,corrimiento)
+	BEGIN 
+			IF (RISING_EDGE(mclk)) THEN 
+				IF (RST = '1') THEN
+					corrimiento <= "10011001";
+				ELSE
+					IF (DIR = '1') THEN
+						corrimiento <= corrimiento(0) & corrimiento (7 DOWNTO 1);
+					ELSIF (DIR = '0') THEN	
+						corrimiento <= corrimiento (6 DOWNTO 0) & corrimiento(7);
+					END IF;
+				END IF;
+			END IF;
+	END PROCESS;
+	
+	LEDS <= not (corrimiento);
+	
+	END Behavioral;
+
